c5d69678d91604554f7bc5ebb02682be15378757
PetitChu <sebcharron0@gmail.com>
2026-01-30 20:07:42 -0500

    refactor/cleanup attribute scripts

--- Assets/_neon/Scripts/Attributes/HelpAttribute.cs
+++ Assets/_neon/Scripts/Attributes/HelpAttribute.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 
 namespace BrainlessLabs.Neon
 {
-
+    /// <summary>
+    /// A custom attribute used to add helpful text to Unity properties in the Inspector.
+    /// Specifies a message that can provide context, instructions, or additional information
+    /// about the associated property.
+    /// </summary>
     public class HelpAttribute : PropertyAttribute
     {
-        public string text;
+        public readonly string text;
 
         public HelpAttribute(string text)
         {
--- Assets/_neon/Scripts/Attributes/ReadOnlyAttribute.cs
+++ Assets/_neon/Scripts/Attributes/ReadOnlyAttribute.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
 
-namespace BrainlessLabs.Neon {
-
-    //class for showing ReadOnly fields in the Unity Inspector
-    public class ReadOnlyProperty : PropertyAttribute {}
-
+namespace BrainlessLabs.Neon
+{
+    /// <summary>
+    /// A custom property attribute used to mark fields in Unity scripts as read-only in the Inspector.
+    /// This attribute is purely for UI purposes and does not enforce immutability or read-only behavior at runtime.
+    /// </summary>
+    public class ReadOnlyProperty : PropertyAttribute { }
 }
